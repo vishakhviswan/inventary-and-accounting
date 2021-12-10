8d12a31de54107fb39c25f71e497619f46690005
vishakhviswan <vishakhviswan1995@gmail.com>
2021-12-10 20:53:43 +0530

    intial Commit

---
+++ src/Pages/Cutting.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Cutting() {
+    return (
+        <div className='cutting_parentDiv'>
+            <div className='cutting_childDiv'>
+                
+            </div>
+            <h1>Cutting</h1>
+
+        </div>
+    )
+}
+
+export default Cutting
