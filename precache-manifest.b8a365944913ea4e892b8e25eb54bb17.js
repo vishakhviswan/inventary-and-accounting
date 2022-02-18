0079410ee39fca6dd38ed60a3f6ed8662b57cbdf
vishakhviswan <vishakhviswan1995@gmail.com>
2022-02-18 11:56:34 +0530

    Updates

---
+++ precache-manifest.b8a365944913ea4e892b8e25eb54bb17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518909c1e564c795ed3d0d73c5b0392c",
+    "url": "/inventary-and-accounting/index.html"
+  },
+  {
+    "revision": "b34ab76c5aa59a951ce2",
+    "url": "/inventary-and-accounting/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "50b879e10b822fb73c3a",
+    "url": "/inventary-and-accounting/static/css/main.624371cf.chunk.css"
+  },
+  {
+    "revision": "b34ab76c5aa59a951ce2",
+    "url": "/inventary-and-accounting/static/js/2.9c44a76b.chunk.js"
+  },
+  {
+    "revision": "3dded4d97446b08dd587e5e42ae2c5d9",
+    "url": "/inventary-and-accounting/static/js/2.9c44a76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b879e10b822fb73c3a",
+    "url": "/inventary-and-accounting/static/js/main.0566b8b0.chunk.js"
+  },
+  {
+    "revision": "179ecd10800f0f0f0b81",
+    "url": "/inventary-and-accounting/static/js/runtime-main.809a16af.js"
+  }
+]);
